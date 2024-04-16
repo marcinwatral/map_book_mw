0f19e5653f62ce452de75fe2e0be03780fb216b9
marcinwatral <163434784+marcinwatral@users.noreply.github.com>
2024-04-16 16:53:14 +0200

    wip: update data structure to list of dictionary

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-imie_1: str="Artur"
-imie_2: str= "Zuzanna"
-imie_3: str= "Kinga"
-imie_4: str= "Marysia"
+users: list=[
 
-online_1: str = "online"
-online_2: str = "offline"
-online_3: str = "online"
-online_4: str = "online"
+    {"name": "Julia","surname":"Gotowiec","posts":1500,},
+    {"name": "Hub","surname":"Sybilski","posts":123456,},
+    {"name": "Adrian","surname":"Dobrzan","posts":2137,}
 
-users: list[str] = [imie_1, imie_2, imie_3, imie_4]
-lista_online: list[str] = [online_1, online_2, online_3, online_4]
-print("Lista znajomych to: ")
-for index, user in enumerate(users):
-    print(f"\tTwoj znajomy {user} jest {lista_online[index]} ")
+]
+print("informacje o Twoich znajomych: ")
+for user in users:
+    print(f'\tTwoj znajomy {user["name"]} {user["surname"]} opublikowal {user["posts"]}.')
