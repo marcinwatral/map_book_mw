18378000ec3c81f33804e2a6a069eafbeca59015
marcinwatral <163434784+marcinwatral@users.noreply.github.com>
2024-04-23 17:17:58 +0200

    fix working from here

---
+++ models/data_source.py
@@ -0,0 +1,7 @@
+users: list = [
+
+    {"name": "Julia", "surname": "Gotowiec", "posts": 1500, },
+    {"name": "Hubert", "surname": "Sybilski", "posts": 123456, },
+    {"name": "Adrian", "surname": "Dobrzański", "posts": 3, },
+    {"name": "Bartek", "surname": "Wyrzykowski", "posts": 666, }
+]
---
+++ utils/crud.py
@@ -0,0 +1,30 @@
+def read_friends(users: list)->None:
+    print("Informacje o twoich znajomych: ")
+    for user in users:
+        print(f'\tTwój znajomy {user["name"]} {user["surname"]} opublikował {user["posts"]} postów.')
+
+def add_user(lista: list) -> None:
+    imie = input("Podaj imię: ")
+    nazwisko = input("Podaj nazwisko: ")
+    liczba_postow = int(input("Podaj liczbę postów użytkownika: "))
+    new_user = {"name": imie, "surname": nazwisko, "posts": liczba_postow, }
+    lista.append(new_user)
+
+def search_user(users: list):
+    imie = input("Podaj imię: ")
+    for user in users:
+        if user["name"] == imie:
+            print(user)
+def remove_user(users: list):
+    imie = input("Podaj imię: ")
+    for user in users:
+        if user["name"] == imie:
+            users.remove(user)
+
+def update_user(users: list):
+    imie = input("Wprowadź imię użytkownika, którego dane chcesz zmienić: ")
+    for user in users:
+        if user["name"] == imie:
+            user["name"] = input("Podaj nowe imię: ")
+            user["surname"] = input("Podaj nowe nazwisko: ")
+            user["posts"] = int(input("Podaj nową liczbę postów: "))
